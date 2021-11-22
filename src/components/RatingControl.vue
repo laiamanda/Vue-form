0f9a8d89653f968dc226495c090ccc6cdd796e23
laiamanda <40805270+laiamanda@users.noreply.github.com>
2021-11-21 22:57:21 -0500

    Upload RatingControl.vue

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,62 @@
+<template>
+    <ul>
+        <li :class="{active: modelValue === 'poor'}"><button type="button" @click="active('poor')"> Poor </button> </li>
+        <li :class="{active: modelValue === 'average'}"> <button type="button" @click="active('average')"> Average </button> </li>
+        <li :class="{active: modelValue === 'great'}">  <button type="button" @click="active('great')"> Great </button> </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['modelValue'],
+    emits: ['update:modelValue'],
+    /*data() {
+        return {
+            activeOption: null
+        };
+    },*/
+    /*computed: {
+        activeOption(){
+            return this.modelValue;
+        }
+    },*/
+    methods : {
+        active(option){
+            //this.activeOption = option;
+            this.$emit('update:modelValue',option);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.active {
+    border-color: purple;
+}
+.active button {
+    color: purple;
+}
+ul{
+    list-style-type: none;
+    margin: 0.5 rem 0;
+    padding: 0;
+    display: flex;
+}
+
+li{
+    margin: 0 1rem;
+    border: 4px solid #ccc;
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+button{
+    font: inherit;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+}
+
+</style>
